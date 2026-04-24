0742433880d7439883c4e3856727e6713785664d
This-is-Mani <207173292+This-is-Mani@users.noreply.github.com>
2026-04-24 12:38:35 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bucket-name"
+    bucket         = "mydevbucket2026"
     key            = "project2/terraform.tfstate"
     region         = "us-east-1"
     # optional:
