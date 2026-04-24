139de99d2d58ea69b773a2cf4809024be075322f
This-is-Mani <207173292+This-is-Mani@users.noreply.github.com>
2026-04-24 13:27:15 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "mydevbucket2026"
-    key            = "project2/terraform.tfstate"
+    key            = "project3/terraform.tfstate"
     region         = "us-east-1"
     # optional:
     # dynamodb_table = "terraform-lock"
